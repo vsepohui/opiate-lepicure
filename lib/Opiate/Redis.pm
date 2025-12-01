875ee88805f78be935059e442b00a90b8bb610be
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2025-12-01 22:40:56 +0500

    fix

--- lib/Opiate/Redis.pm
+++ lib/Opiate/Redis.pm
@@ -4,7 +4,7 @@ use 5.022;
 use warnings;
 
 use utf8;
-use Redis::Fast;
+use Redis;
 
 
 sub new {
@@ -14,7 +14,7 @@ sub new {
 }
 
 sub redis {
-	state $redis = Redis::Fast->new(server => '127.0.0.1:6379', encoding => undef);
+	state $redis = Redis->new(server => '127.0.0.1:6379', encoding => undef);
 	return $redis;
 }
 
