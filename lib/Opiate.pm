c7f88eb551d269a7992306889bd566ded6965cc4
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-09-16 04:33:59 +0500

    Cleanup session

--- lib/Opiate.pm
+++ lib/Opiate.pm
@@ -41,12 +41,12 @@ sub startup {
 					return $c->page_404;
 				}
 			} else {
-				$self->{session} = {};
-				#$self->session(expires => 1);
+				$self->session(alias => undef);
+				$self->session(expires => 1);
 			}
 		} else {
-			$self->{session} = {};
-			#$self->session(expires => 1);
+			$self->session(alias => undef);
+			$self->session(expires => 1);
 		}
 
 		$c->stash('user' => $user);
